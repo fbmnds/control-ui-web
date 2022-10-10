ee4c77543ec597afe14d53dfb5866fa45720bf73
fbmnds7 <fbmnds7@fn.de>
2022-10-10 20:37:44 +0200

    Improve backup file handling

--- src/backup.lisp
+++ src/backup.lisp
@@ -3,47 +3,51 @@
 (defun fmt-rfc3339-now ()
   (local-time:format-rfc3339-timestring nil (local-time:now)))
 
-(defun select-last-ts-cmd ()
-  (str+ "/usr/bin/sqlite3 "
-        *backup-file*
-        " 'select ts from heating order by ts desc limit 1;'"))
+(defun last-backup ()
+  (let ((cmd (str+ "/bin/ls " *backup-dir*
+                   " | /usr/bin/sort | /usr/bin/tail -n 1")))
+    (string-trim '(#\Linefeed #\Newline)
+                 (str+ *backup-dir*
+                       (uiop:run-program cmd :force-shell t :output :string)))))
 
-(defun run-select-last-ts ()
-  (let ((cmd (select-last-ts-cmd)))
+(defun select-last-ts ()
+  (let ((cmd (str+ "/usr/bin/sqlite3 "
+                   (last-backup)
+                   " 'select ts from heating order by ts desc limit 1;'")))
     (uiop:run-program cmd :force-shell t
                           :output '(:string :stripped t))))
 
-(defun fetch-ssh-cmd (last-ts)
-  (str+ "/usr/bin/ssh " *ssh-prod-parameter*
-        "\"/usr/bin/sqlite3 -json "
-        *prod-file*
-        " 'select * from heating"
-        " where ts > \\\"" last-ts "\\\""
-        " order by ts asc;'\""))
-
 (defun fetch-delta ()
   (ignore-errors
-   (let* ((last-ts (run-select-last-ts))
-          (cmd (fetch-ssh-cmd last-ts))
+   (let* ((last-ts (select-last-ts))
+          (cmd (str+ "/usr/bin/ssh " *ssh-prod-parameter*
+                     "\"/usr/bin/sqlite3 -json "
+                     *prod-file*
+                     " 'select * from heating"
+                     " where ts > \\\"" last-ts "\\\""
+                     " order by ts asc;'\""))
           (delta (uiop:run-program cmd :force-shell t
                                        :output '(:string :stripped t))))
      (when (stringp delta) (yason:parse delta)))))
 
-(defun insert-delta ()
+(defun run-fetch-insert-delta ()
   (let ((delta (fetch-delta))
         (db (str+ *backup-dir* (fmt-rfc3339-now) "-heating.db")))
     (handler-case
-        (progn
-          (uiop:run-program (str+ "/bin/cp " *backup-file* " " db))
-          (let ((connected-db (sqlite:connect db)))
-            (dolist (i delta)
-              (ignore-errors
+         (progn
+           (uiop:run-program (str+ "/bin/cp " (last-backup) " " db)
+                             :force-shell t)
+           (let ((connected-db (sqlite:connect db)))
+             (dolist (i delta)
                (execute-non-query
                 connected-db (str+ "insert into heating (ts,temp,hum,state)"
                                    " values (?,round(?,2),round(?,2),?)")
                 (gethash "ts" i) (gethash "temp" i)
-                (gethash "hum" i) (gethash "state" i))))))
-      (error (c) c))))
+                (gethash "hum" i) (gethash "state" i))))
+           db)
+      (error (c)
+        (uiop:run-program (str+ "/bin/rm " db) :force-shell t)
+        c))))
 
 (defun run-sshfs-backup-data ()
   (let* ((db-file-name
@@ -51,9 +55,7 @@
          (cp (str+ "/bin/cp " *sshfs-prod-file* " " db-file-name)))
     (multiple-value-bind (_1 rc _2)
         (uiop:run-program cp :force-shell t)
-      (if rc
-          (values _1 rc _2)
-          (setf *backup-file* db-file-name)))))
+      (if rc (values _1 rc _2) db-file-name))))
 
 
 
